d545d74452a06d2758e63ba74e09ff477635e22d
Rohan Sharma <rohan.sharma@talview.com>
2023-08-17 15:16:47 +0530

    fix version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023081601;
+$plugin->version = 2023081701;
 $plugin->requires = 2020061500;
-$plugin->release = '1.1.0 (Build: 2023081601)';
+$plugin->release = '1.1.0 (Build: 2023081701)';
 $plugin->component = 'quizaccess_proctor';
 $plugin->maturity = MATURITY_STABLE;
 
