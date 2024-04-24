d0880162ce110da9c3dc3f2cf3794de7629c1fd6
Rohan Sharma Sitoula <rohan.sharma@talview.com>
2024-04-24 09:41:08 +0530

    version update

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2024041901;
+$plugin->version = 2024042401;
 $plugin->requires = 2020061500;
-$plugin->release = '1.4.0 (Build: 2024041901)';
+$plugin->release = '1.4.0 (Build: 2024042401)';
 $plugin->component = 'quizaccess_proctor';
 $plugin->maturity = MATURITY_STABLE;
 
