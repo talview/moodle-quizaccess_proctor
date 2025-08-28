7e6a0847f531a3cd8bd9761f2ddf74fd636f1919
rohansharmaistoula <rohan.sharma@talview.com>
2025-08-28 15:06:56 +0530

    fix: replace ckeditor with moodle default rich text editor for instructions

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2025032102;
+$plugin->version = 2025082501;
 $plugin->requires = 2020061500;
-$plugin->release = '1.5.1 (Build: 2025032102)';
+$plugin->release = '1.5.2 (Build: 2025082501)';
 $plugin->component = 'quizaccess_proctor';
 $plugin->maturity = MATURITY_STABLE;
 
